6413885f372cc0fbaab70d5fba62c8c8f263f3bd
KOTFAN <kotfan.empire@gmail.com>
2025-02-23 19:57:21 +0200

    add Grasshopper - Summation

--- 8kyu.js
+++ 8kyu.js
@@ -423,3 +423,17 @@ function removeChar(str) {
 function repeatStr(n, s) {
    return s.repeat(n);
 }
+
+
+
+
+//Grasshopper - Summation
+
+function summation(num) {
+   let sum = 0;
+
+   for (let i = num; i > 0; i--) {
+      sum += i
+   }
+   return sum
+}

8da2a812d19bc736197cf78ce6abbd27fabf1cdc
KOTFAN <kotfan.empire@gmail.com>
2025-02-23 11:46:35 +0200

    add Quadratic Coefficients Solver

--- 8kyu.js
+++ 8kyu.js
@@ -180,3 +180,9 @@ const flip = (d, a) => {
 
 
 //========================================================
+
+
+//Quadratic Coefficients Solver 
+function quadratic(x1, x2) {
+   return [1, -x1 - x2, x1 * x2];
+}

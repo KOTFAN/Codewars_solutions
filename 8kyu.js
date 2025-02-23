6621c19ed09b3e24dd409ebbd6d4b7ac2e0a2d53
KOTFAN <kotfan.empire@gmail.com>
2025-02-23 19:41:35 +0200

    add Convert boolean values to strings 'Yes' or 'No'.

--- 8kyu.js
+++ 8kyu.js
@@ -383,3 +383,13 @@ function solution(str) {
 function numberToString(num) {
    return `${num}`
 }
+
+
+
+//Convert boolean values to strings 'Yes' or 'No'.
+
+
+
+function boolToWord(bool) {
+   return bool ? "Yes" : "No"
+}

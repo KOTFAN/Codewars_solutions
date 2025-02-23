195f8ef7058ba71be08c5a1583c790e67faab294
KOTFAN <kotfan.empire@gmail.com>
2025-02-23 20:02:23 +0200

    add Find the smallest integer in the array

--- 8kyu.js
+++ 8kyu.js
@@ -437,3 +437,19 @@ function summation(num) {
    }
    return sum
 }
+
+
+//Find the smallest integer in the array
+
+
+function findSmallestInt(arr) {
+   let min = arr[0];
+
+   for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < min) {
+         min = arr[i];
+      }
+   }
+
+   return min;
+}

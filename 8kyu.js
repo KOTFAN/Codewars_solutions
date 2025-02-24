493bcfb13afb76a0e94dec4f78760680ef7851d1
KOTFAN <kotfan.empire@gmail.com>
2025-02-25 01:22:51 +0200

    add Beginner - Reduce but Grow

--- 8kyu.js
+++ 8kyu.js
@@ -731,3 +731,13 @@ function fakeBin(x) {
 function check(a, x) {
    return a.includes(x)
 }
+
+//Beginner - Reduce but Grow
+function grow(x) {
+   let res = 1;
+   for (let i = 0; i < x.length; i++) {
+      res *= x[i]
+   }
+   return res;
+
+}

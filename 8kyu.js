ac83d47fb1b90bba20dc1c4a14c7ae7bb15c7faa
KOTFAN <kotfan.empire@gmail.com>
2025-02-26 23:09:00 +0200

    add Reversed Words

--- 8kyu.js
+++ 8kyu.js
@@ -918,3 +918,10 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
    return ((givenMass1 / molarMass1) + (givenMass2 / molarMass2)) * 0.082 * (temp + 273.15) / volume
 }
+
+
+
+//Reversed Words
+function reverseWords(str) {
+   return str.split(' ').reverse().join(' ');
+}

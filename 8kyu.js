6f6e176ed050e89d33ee4cd76428e8954e3a4da4
KOTFAN <kotfan.empire@gmail.com>
2025-02-27 23:29:55 +0200

    add Will there be enough space?

--- 8kyu.js
+++ 8kyu.js
@@ -1053,3 +1053,8 @@ function removeEveryOther(arr) {
    }
    return res;
 }
+
+//Will there be enough space?
+function enough(cap, on, wait) {
+   return cap - on >= wait ? 0 : wait - (cap - on)
+}

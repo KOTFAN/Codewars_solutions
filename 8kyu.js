75cd696397bef528b8aaf64a0384f88d4eb89846
KOTFAN <kotfan.empire@gmail.com>
2025-03-10 12:11:36 +0200

    add Student's Final Grade

--- 8kyu.js
+++ 8kyu.js
@@ -1297,3 +1297,17 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
 }
 
+
+//Student's Final Grade
+function finalGrade(exam, projects) {
+   if (exam > 90 || projects > 10) {
+      return 100
+   } else if (exam > 75 && projects >= 5) {
+      return 90
+   }
+   else if (exam > 50 && projects >= 2) {
+      return 75
+   }
+   console.log(exam, projects)
+   return 0
+}

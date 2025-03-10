e77cbe5e6e919f05c09011d83f1997552607631d
KOTFAN <kotfan.empire@gmail.com>
2025-03-10 15:36:53 +0200

    add Who likes it?

--- 6kyu.js
+++ 6kyu.js
@@ -20,3 +20,25 @@ function solution(number) {
 function spinWords(string) {
    return string.split(" ").map(w => w.length > 4 ? w.split("").reverse().join("") : w).join(" ")
 }
+
+
+//Who likes it?
+function likes(names) {
+   const nLen = names.length;
+   if (nLen === 0) {
+      return "no one likes this"
+
+   } else if (nLen === 1) {
+      return `${names[0]} likes this`
+
+   } else if (nLen === 2) {
+      return `${names[0]} and ${names[1]} like this`
+
+   } else if (nLen === 3) {
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+
+   }
+   return `${names[0]}, ${names[1]} and ${nLen - 2} others like this`
+
+
+}

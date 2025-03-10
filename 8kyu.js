e21e1880129d6c7ab1e671204e5afc0de71a69a8
KOTFAN <kotfan.empire@gmail.com>
2025-03-10 12:04:43 +0200

    add I love you, a little , a lot, passionately ... not at all

--- 8kyu.js
+++ 8kyu.js
@@ -1276,4 +1276,20 @@ function greet(language) {
 
 
 
+//I love you, a little , a lot, passionately ... not at all
+
+function howMuchILoveYou(n) {
+   //i love mod n
+   const answs = {
+      0: "not at all",
+      1: "I love you",
+      2: "a little",
+      3: "a lot",
+      4: "passionately",
+      5: "madly",
+   }
+   return answs[n % 6]
+}
+
+
 

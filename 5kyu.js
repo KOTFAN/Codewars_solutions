5938dadb8107b77b29c51de6fbe5dba20eee29e4
KOTFAN <kotfan.empire@gmail.com>
2025-03-12 14:15:44 +0200

    add Human Readable Time

--- 5kyu.js
+++ 5kyu.js
@@ -16,3 +16,14 @@ function moveZeros(arr) {
 function pigIt(str) {
    return str.split(' ').map((v) => v[0].charCodeAt() > 64 ? v.slice(1) + v[0] + 'ay' : v).join(' ')
 }
+
+//Human Readable Time
+function humanReadable(seconds) {
+
+   const hours = seconds / 3600
+   const minutes = 60 * (hours - Math.floor(hours))
+   const sec = 60 * (minutes - Math.floor(minutes))
+
+   return `${Math.floor(hours) > 9 ? Math.floor(hours) : '0' + Math.floor(hours)}:${Math.floor(minutes) > 9 ? Math.floor(minutes) : '0' + Math.floor(minutes)}:${Math.round(sec) > 9 ? Math.round(sec) : '0' + Math.round(sec)}`;
+
+}

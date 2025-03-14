856fa72c23997b749a31c3234a29aec2e6c93d8d
KOTFAN <kotfan.empire@gmail.com>
2025-03-14 22:16:21 +0200

    add Stringy Strings

--- 8kyu.js
+++ 8kyu.js
@@ -1349,3 +1349,18 @@ const square = (a) => a ** 2
 function squareOrSquareRoot(array) {
    return array.map(v => v ** (1 / 2) % 1 ? v * v : v ** (1 / 2));
 }
+
+//Stringy Strings
+function stringy(size) {
+   let res = ''
+   for (let i = 0; i < size; i++) {
+      if (i % 2 === 0) {
+         res += '1'
+      }
+      else {
+         res += '0'
+      }
+   }
+
+   return res
+}

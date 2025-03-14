3d06bae71478f41b5e701b3138b6a095fbbc0fe8
KOTFAN <kotfan.empire@gmail.com>
2025-03-14 18:49:55 +0200

    add Tribonacci Sequence

--- 6kyu.js
+++ 6kyu.js
@@ -199,3 +199,19 @@ function order(words) {
    }
    return res.join(' ')
 }
+
+//Tribonacci Sequence
+function tribonacci(signature, n) {
+   const res = []
+
+
+   for (let i = 0; i < n; i++) {
+      if (i < 3) {
+         res.push(signature[i])
+      } else {
+         res.push(res[i - 3] + res[i - 2] + res[i - 1])
+      }
+
+   }
+   return res
+}

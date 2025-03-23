f968377796e8799820ac8f8b2edca5b3bcc81eff
KOTFAN <kotfan.empire@gmail.com>
2025-03-23 18:17:18 +0200

    add Fix your code before the garden dies!

--- 8kyu.js
+++ 8kyu.js
@@ -1619,3 +1619,9 @@ function convertToCelsius(temperature) {
    const celsius = (temperature - 32) * 5 / 9
    return celsius
 }
+
+//Fix your code before the garden dies!
+function rainAmount(mm) {
+   if (mm < 40) return `You need to give your plant ${40 - mm}mm of water`
+   else return "Your plant has had more than enough water for today!"
+}

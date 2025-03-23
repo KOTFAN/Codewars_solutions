0ef10555b00c15d51afeeb06ece7a3a4fa2101e7
KOTFAN <kotfan.empire@gmail.com>
2025-03-23 18:25:07 +0200

    add Swap Values

--- 8kyu.js
+++ 8kyu.js
@@ -1625,3 +1625,7 @@ function rainAmount(mm) {
    if (mm < 40) return `You need to give your plant ${40 - mm}mm of water`
    else return "Your plant has had more than enough water for today!"
 }
+//Swap Values
+function swapValues(arr) {
+   return arr.reverse()
+}

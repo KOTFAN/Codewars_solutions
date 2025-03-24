a990b9859ec2c6a010ddc58aebfb5ba0d5bc0696
KOTFAN <kotfan.empire@gmail.com>
2025-03-25 00:54:34 +0200

    add Plural

--- 8kyu.js
+++ 8kyu.js
@@ -1748,3 +1748,6 @@ const reverse = (str) => str.split(' ').reverse().join(' ')
 function mergeArrays(arr1, arr2) {
    return Array.from(new Set(arr1.concat(arr2).sort((a, b) => a - b)))
 }
+
+//Plural
+const plural = n => n !== 1

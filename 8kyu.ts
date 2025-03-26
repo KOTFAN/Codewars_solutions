118e168cba4a463fa87a725530ed84ba5dd621cc
KOTFAN <kotfan.empire@gmail.com>
2025-03-26 19:53:35 +0200

    add Grasshopper - Function syntax debugging

--- 8kyu.ts
+++ 8kyu.ts
@@ -3,3 +3,8 @@
 export function toBinary(n: number): number {
    return Number(n.toString(2));
 }
+
+//Grasshopper - Function syntax debugging
+export function main(verb: string, noun: string): string {
+   return verb + noun
+}

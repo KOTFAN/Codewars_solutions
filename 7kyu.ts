514b1bcd63aab01f944c9ab661c38a4ef8904faf
KOTFAN <kotfan.empire@gmail.com>
2025-03-27 23:26:01 +0200

    add Ninja vs Samurai: Strike

--- 7kyu.ts
+++ 7kyu.ts
@@ -59,4 +59,17 @@ export class Dinglemouse {
 
 }
 
+//Ninja vs Samurai: Strike
+export class Warrior {
+   private name: string;
+   public health: number;
+   constructor(name: string) {
+      this.name = name;
+      this.health = 100;
+   }
+
+   strike(enemy: Warrior, swings: number) {
+      enemy.health = Math.max(0, enemy.health - (swings * 10));
+   }
+}
 

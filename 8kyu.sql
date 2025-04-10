83c39fc7254ca730e7279823813d39e2a114899e
KOTFAN <kotfan.empire@gmail.com>
2025-04-10 21:50:22 +0300

    add 1. Find all active students

--- 8kyu.sql
+++ 8kyu.sql
@@ -4,3 +4,7 @@ SELECT price * amount AS total FROM items
 SELECT SQRT(number1) AS root, LOG10(number2) AS log FROM decimals 
 --Easy SQL: Rounding Decimals
 SELECT FLOOR(number1)as number1,CEIL(number2)as number2 FROM decimals
+--Easy SQL: Convert to Hexadecimal
+SELECT to_hex(legs) as legs, to_hex(arms) as arms FROM monsters
+--1. Find all active students
+SELECT * FROM students WHERE IsActive

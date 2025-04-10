34879bb53684dfc1575c2b42c5fa821eb27758ea
KOTFAN <kotfan.empire@gmail.com>
2025-04-10 21:33:09 +0300

    add Easy SQL: Rounding Decimals

--- 8kyu.sql
+++ 8kyu.sql
@@ -2,3 +2,5 @@
 SELECT price * amount AS total FROM items
 --Easy SQL: Square Root and Log
 SELECT SQRT(number1) AS root, LOG10(number2) AS log FROM decimals 
+--Easy SQL: Rounding Decimals
+SELECT FLOOR(number1)as number1,CEIL(number2)as number2 FROM decimals

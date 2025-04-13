c3235280bb206274a39090c40b2ecc3031f85251
KOTFAN <kotfan.empire@gmail.com>
2025-04-13 16:44:58 +0300

    add Easy SQL: Bit Length

--- 7kyu.sql
+++ 7kyu.sql
@@ -25,3 +25,5 @@ WHERE ((quality1='evil' AND quality2='cunning'))
 OR (quality1='brave' AND quality2<>'evil') 
 OR (quality1='studious' OR quality2='intelligent')
 OR (quality1='hufflepuff' OR quality2='hufflepuff')
+
+SELECT id,LENGTH(name)*8 as name,birthday,LENGTH(race) *8 as race FROM demographics

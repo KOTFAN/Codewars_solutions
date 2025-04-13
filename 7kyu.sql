f2edad76d82d9efc91ab4ab45c1ab052fc138011
KOTFAN <kotfan.empire@gmail.com>
2025-04-13 14:19:23 +0300

    add Countries Capitals for Trivia Night (SQL for Beginners #6)

--- 7kyu.sql
+++ 7kyu.sql
@@ -4,3 +4,8 @@ ORDER BY copies_sold DESC
 LIMIT 5
 --SQL: Concatenating Columns
 SELECT CONCAT_WS(' ',prefix,first,last,suffix) AS title FROM names
+--Countries Capitals for Trivia Night (SQL for Beginners #6)
+SELECT capital FROM countries 
+WHERE (continent = 'Africa' OR continent = 'Afrika') AND country LIKE 'E%' 
+ORDER BY capital ASC
+LIMIT 3

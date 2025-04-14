9baf3eb2e8a0bd8773257025d5611f672c704a3d
KOTFAN <kotfan.empire@gmail.com>
2025-04-14 21:36:56 +0300

    add SQL Basics: Simple JOIN

--- 7kyu.sql
+++ 7kyu.sql
@@ -39,3 +39,19 @@ SELECT
   LENGTH(characteristics) AS arms,
   LENGTH(CAST(id AS TEXT)) AS characteristics
 FROM monsters;
+
+--SQL Basics: Simple JOIN
+SELECT 
+
+products.id, 
+products.name, 
+products.isbn, 
+products.company_id, 
+products.price,
+companies.name AS company_name
+
+FROM products
+
+JOIN companies
+
+ON products.company_id = companies.id;

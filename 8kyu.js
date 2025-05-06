306f0965c441071e0557320a30783e66fbac10df
KOTFAN <kotfan.empire@gmail.com>
2025-05-07 00:38:54 +0300

    add Finish Guess the Number Game

--- 8kyu.js
+++ 8kyu.js
@@ -1875,3 +1875,24 @@ class Person {
       return "Welcome to Planet Earth " + raceName
    }
 }
+
+//Finish Guess the Number Game
+class Guesser {
+   constructor(number, lives) {
+      this.number = number;
+      this.lives = lives;
+   }
+   guess(n) {
+      if (this.lives <= 0) {
+         throw new Error('already dead');
+
+      }
+
+      if (this.number === n) {
+         return true
+      }
+      this.lives--
+
+      return false
+   }
+}

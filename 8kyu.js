13b8f6449da7d5234795bf47317e1638761cf16f
KOTFAN <kotfan.empire@gmail.com>
2025-05-07 00:42:53 +0300

    add Basic subclasses - Adam and Eve

--- 8kyu.js
+++ 8kyu.js
@@ -1896,3 +1896,25 @@ class Guesser {
       return false
    }
 }
+//Basic subclasses - Adam and Eve
+class God {
+   static create() {
+      return [new Man('Adam'), new Woman('Eve')]
+   }
+}
+
+class Human {
+   constructor(name) {
+      this.name = name
+   }
+}
+class Man extends Human {
+   constructor(name) {
+      super(name)
+   }
+}
+class Woman extends Human {
+   constructor(name) {
+      super(name)
+   }
+}

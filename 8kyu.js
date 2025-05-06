5fd613d8be8a0b183b8f941d8ff4aa40bd661100
KOTFAN <kotfan.empire@gmail.com>
2025-05-06 23:46:12 +0300

    add Classy Classes

--- 8kyu.js
+++ 8kyu.js
@@ -1848,3 +1848,14 @@ class Cube {
 
    }
 }
+//Classy Classes
+class Person {
+   constructor(name, age) {
+      this.name = name;
+      this.age = age;
+   }
+   get info() {
+      return `${this.name}s age is ${this.age}`
+   }
+
+}

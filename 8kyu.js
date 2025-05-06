2e6c98fa82bdc63bdd2384ca93426c232c7fac43
KOTFAN <kotfan.empire@gmail.com>
2025-05-06 23:50:32 +0300

    add Fun with ES6 Classes #1 - People, people, people

--- 8kyu.js
+++ 8kyu.js
@@ -1859,3 +1859,19 @@ class Person {
    }
 
 }
+//Fun with ES6 Classes #1 - People, people, people
+class Person {
+   constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.gender = gender;
+   }
+   sayFullName() {
+      return `${this.firstName} ${this.lastName}`
+   }
+
+   static greetExtraTerrestrials(raceName) {
+      return "Welcome to Planet Earth " + raceName
+   }
+}

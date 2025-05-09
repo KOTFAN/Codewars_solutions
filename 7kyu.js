aee0b1cf4ab2f293ccaecc5762fddbbba083eae4
KOTFAN <kotfan.empire@gmail.com>
2025-05-10 00:02:28 +0300

    add SantaClausable Interface

--- 7kyu.js
+++ 7kyu.js
@@ -371,3 +371,13 @@ class Singleton {
       return this;
    }
 };
+
+//SantaClausable Interface
+function isSantaClausable(obj) {
+   if (typeof obj.sayHoHoHo === 'function' &&
+      typeof obj.distributeGifts === 'function' &&
+      typeof obj.goDownTheChimney === 'function') {
+      return true;
+   }
+   return false;
+}

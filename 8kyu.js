82b95eaf26f63600665ed0aaf4a623b0f0b435f3
KOTFAN <kotfan.empire@gmail.com>
2025-05-14 21:59:13 +0300

    add Grasshopper - Object syntax debug

--- 8kyu.js
+++ 8kyu.js
@@ -2046,3 +2046,21 @@ websites.push("codewars")
 const arrowFunc = function (arr) {
    return arr.map(n => String.fromCharCode(n)).join('');
 }
+
+//Grasshopper - Object syntax debug
+let rooms = {
+   first: {
+      description: 'This is the first room',
+      items: {
+         chair: 'The old chair looks comfortable',
+         lamp: 'This lamp looks ancient',
+      }
+   },
+   second: {
+      description: 'This is the second room',
+      items: {
+         couch: 'This couch looks like it would hurt your back',
+         table: 'On the table there is an unopened bottle of water',
+      }
+   },
+}

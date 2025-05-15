2975d530e0ae4fe3c5e07d150871817281b7352b
KOTFAN <kotfan.empire@gmail.com>
2025-05-15 22:17:11 +0300

    add Ones and Zeros

--- 7kyu.js
+++ 7kyu.js
@@ -583,3 +583,15 @@ function sysNums(n, sys) {
    }
    return Number(newNum)
 }
+
+//Ones and Zeros
+const binaryArrayToNumber = arr => {
+   let res = 0;
+
+   for (let i = arr.length - 1; i >= 0; i--) {
+      res += arr[i] * 2 ** (arr.length - i - 1)
+      console.log(arr[i])
+   }
+
+   return res
+};

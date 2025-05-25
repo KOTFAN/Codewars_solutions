e75a4d0ad7848dd49e3d693d8722c1d8ae843f2c
KOTFAN <kotfan.empire@gmail.com>
2025-05-25 21:12:36 +0300

    add Points of Reflection

--- 8kyu.js
+++ 8kyu.js
@@ -2077,3 +2077,8 @@ function calculateAge(startAge, finAge) {
 function circleCircumference(circle) {
    return (2 * Math.PI * circle.radius)
 }
+
+//Points of Reflection
+function symmetricPoint([p1, p2], [q1, q2]) {
+   return [2 * q1 - p1, 2 * q2 - p2]
+}

7f33276c4d1fb82375dd344e810d9e399b68b4ea
KOTFAN <kotfan.empire@gmail.com>
2025-05-30 16:54:11 +0300

    add split strings

--- 6kyu.js
+++ 6kyu.js
@@ -279,3 +279,16 @@ const decodeMorse = function (morseCode) {
    // Your code here
    // You can use MORSE_CODE[morse]
 }
+
+//Split Strings
+function solution(str) {
+   const result = []
+   for (let i = 0; i < str.length; i = i + 2) {
+      if (i + 1 < str.length) {
+         result.push(str[i] + str[i + 1])
+      } else {
+         result.push(str[i] + '_')
+      }
+   }
+   return result
+}

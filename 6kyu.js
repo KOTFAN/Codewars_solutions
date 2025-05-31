7f2b772867ecc19335bf05608bd980257253a99b
KOTFAN <kotfan.empire@gmail.com>
2025-05-31 19:13:38 +0300

    add Find the missing letter

--- 6kyu.js
+++ 6kyu.js
@@ -345,3 +345,15 @@ function sortArray(array) {
    }
    return res
 }
+
+//Find the missing letter
+function findMissingLetter(array) {
+
+   for (let i = 0; i < array.length - 1; i++) {
+      if ((array[i + 1].charCodeAt() - array[i].charCodeAt()) === 2) {
+         const code = array[i].charCodeAt() + 1
+         return String.fromCharCode(code)
+      }
+   }
+
+}

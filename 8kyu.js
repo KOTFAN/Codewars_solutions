1a9a9f29e7f1f9da31195e418ab422d872b8bf42
KOTFAN <kotfan.empire@gmail.com>
2025-06-20 18:59:58 +0300

    Training JS #4: Basic data types--Array

--- 8kyu.js
+++ 8kyu.js
@@ -2218,3 +2218,27 @@ function take(arr, n) {
 function findAverage(nums) {
    return nums.reduce((a, v) => a + v, 0) / nums.length
 }
+
+
+function getLength(arr) {
+   //return length of arr
+   return arr.length
+}
+function getFirst(arr) {
+   //return the first element of arr
+   return arr[0]
+}
+function getLast(arr) {
+   //return the last element of arr
+   return arr.at(-1)
+}
+function pushElement(arr) {
+   var el = 1;
+   arr.push(el)
+   return arr
+}
+function popElement(arr) {
+   arr.pop()
+
+   return arr
+}

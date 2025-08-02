76f101e9275137fd778b164d30a1b7e9c769757c
KOTFAN <kotfan.empire@gmail.com>
2025-08-02 19:14:23 +0300

    add Alan Partridge II - Apple Turnover

--- 8kyu.js
+++ 8kyu.js
@@ -2284,3 +2284,9 @@ function sumOfDifferences(arr) {
 
   return res;
 }
+
+//Alan Partridge II - Apple Turnover
+function apple(x) {
+  if (Number(x) ** 2 >= 1000) return "It's hotter than the sun!!";
+  return "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
